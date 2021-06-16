7dfe45434e58c70da9c2c41874f159f542e4db18
sanjoli63 <sanjoligo6mar@gmail.com>
2021-06-16 12:31:30 +0530

    cin.getline

---
+++ 3_character_array/cin_getline.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int length (char input[])
+ {
+     int count =0;
+     for(int i=0;input[i]!= '\0';i++)
+     {count++;
+     }
+     return count;
+ }
+int main()
+{
+    char name[100];
+    cout<<"Enter name";
+    cin.getline(name,100);
+    cout<<name;
+}
