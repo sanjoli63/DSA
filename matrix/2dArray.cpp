213639301b5753304d6232da10c69074f77ca2c5
sanjoli63 <sanjoligo6mar@gmail.com>
2021-06-16 13:16:12 +0530

    matrixcolumn  print

--- matrix/2dArray.cpp
+++ matrix/2dArray.cpp
@@ -13,12 +13,22 @@ int main()
             cin>>a[i][j];
         }
     }
-
+    // rowise
     for(int i=0;i<m;i++)
     {
         for(j=0;j<n;j++)
         {
             cout<<a[i][j];
+        }
+        cout<<endl;
+    }
+    //columnwise
+    // rowise
+    for(int i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            cout<<a[j][i];
         }
         cout<<endl;
     }
