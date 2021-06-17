04aa12ec4d7946c02df5fdec6d5af53efbc878b8
sanjoli63 <sanjoligo6mar@gmail.com>
2021-06-17 13:24:51 +0530

    string input

---
+++ 5_string/input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    getline(cin,s);
+    cout<< s<<endl;
+    string s2= s.substr(2);
+    cout<< s2<< endl;
+}
