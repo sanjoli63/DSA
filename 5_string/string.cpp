e30300918ff914a1891d411438ae7195c61c179f
sanjoli63 <sanjoligo6mar@gmail.com>
2021-06-17 13:00:33 +0530

    string

---
+++ 5_string/string.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string s ="abc";
+    cout<<s<<endl;
+
+    //dynamic string
+    string *sp = new string;
+    *sp ="mno";
+    cout<<sp<<endl;
+    cout<<*sp<<endl;
+}
