483b91fb21994716c828c7aa7714f2c1e4f543d3
sanjoli63 <sanjoligo6mar@gmail.com>
2021-06-17 13:39:07 +0530

    string input

--- 5_string/input.cpp
+++ 5_string/input.cpp
@@ -6,6 +6,17 @@ int main()
     string s;
     getline(cin,s);
     cout<< s<<endl;
-    string s2= s.substr(2);
+    string s2= s.substr(2); //substring of s from index 2
     cout<< s2<< endl;
+    int index = s2.find("a"); //find index of a in string s2
+    cout << index << endl;
+
+    int a = 1234;
+     string b =to_string(a); //convert into into string
+     cout<<b<<endl;
+
+     // cast string to int
+    int num=atoi("22");
+    cout<<num;
+    return 0;
 }
